73d416f4e4061ebbadb49ea6195347a0bcc51238
son9912 <son9912@gmail.com>
2018-04-02 19:57:13 +0700

    Remove old graph to R file

---
+++ package-downloader.R
@@ -0,0 +1,5 @@
+if (!require("fivethirtyeight")) install.packages("fivethirtyeight");
+if (!require("tidyverse")) install.packages("tidyverse");
+if (!require("gapminder")) install.packages("gapminder");
+if (!require("knitr")) install.packages("knitr");
+if (!require("tidyr")) install.packages("tidyr");
